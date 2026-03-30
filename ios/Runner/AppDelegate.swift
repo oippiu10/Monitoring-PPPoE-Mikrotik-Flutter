243e026242d8c222b79e148ddb0bb6dbbd40e92c
oippiu10 <oippiu10@gmail.com>
2026-03-31 00:33:24 +0700

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,12 +8,6 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    
-    // Registrasi otomatis tanpa harus import modul di atas
-    if let workmanagerPlugin = self.registrar(forPlugin: "WorkmanagerPlugin") {
-       // Workmanager registrasi otomatis melalui GeneratedPluginRegistrant
-    }
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
