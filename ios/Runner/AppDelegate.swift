a904c3c423fae4abbce7fc658380f334d7b551e3
Hasan Mahfudh <layar89000@gmail.com>
2026-04-02 00:20:51 +0700

    fix(ios): add window getter in AppDelegate to prevent flutter_contacts SIGTRAP crash

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,21 @@ import UIKit
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override var window: UIWindow? {
+    get {
+      if let w = super.window { return w }
+      if #available(iOS 13.0, *) {
+        return UIApplication.shared.connectedScenes
+          .compactMap { $0 as? UIWindowScene }
+          .flatMap { $0.windows }
+          .first { $0.isKeyWindow }
+          ?? UIApplication.shared.connectedScenes
+          .compactMap { $0 as? UIWindowScene }
+          .flatMap { $0.windows }.first
+      }
+      return UIApplication.shared.windows.first
+    }
+    set { super.window = newValue }
+  }
 }
